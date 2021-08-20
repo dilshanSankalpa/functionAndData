fdb465cd9cb36636d45d2f26d0effb8a680b3128
dilshanSankalpa <dilshansankalpa98@gmail.com>
2021-08-20 22:45:54 +0530

    final push everything run smoothly

--- function_and_data.scala
+++ function_and_data.scala
@@ -1,4 +1,4 @@
-object function_and_data{
+object function_and_data extends App{
 
     //rational number problem
 
@@ -31,18 +31,21 @@ object function_and_data{
 println("Rational number problem");
 
 val a = new Rational(2,3);
-printtln(a.neg.toString());
+println("negation : of " + a);
+println(a.neg.toString());
 
 val x = new Rational(3,4);
 val y = new Rational(5,8);
 val z = new Rational(2,7);
 val w = x-y-z;
 
+println("x-y-z where x=3/4, y=5/8, z=2/7");
 println(w.toString());
 
 
-//Account problem
 
+//Account problem
+println("\nAccount problem");
 
 
 class Account (y: Int , z : Double){
@@ -50,9 +53,9 @@ class Account (y: Int , z : Double){
     val accountNumber : Int = y
     var balance : Double = z
 
+    def deposit( amount : Double ):Unit = this.balance += amount
+    
     def withdraw( amount : Double ):Unit = this.balance -= amount
-
-    def deposit ( amount : Double ):Unit = this.balance += amount
 
     def transfer( amount : Double , that : Account ): Unit = {
         this.balance =  this.balance - amount
@@ -61,41 +64,46 @@ class Account (y: Int , z : Double){
 
 
     @Override
-    override def toString() : String = "Account Number : " + this.accountNumber + "\n Balance : " + this.balance
+    override def toString() : String = "Account Number : " + this.accountNumber + " Balance : " + this.balance + "\n"
 
 }
 
 //money transfer
 val acc1 = new Account(100, 40000);
 val acc2 = new Account(101, 50);
-accc1.transfer(50, acc2);
-println(acc1).toString;
+acc1.transfer(50, acc2);
+println(acc2).toString;
 
 var bank : List[Account] = List(new Account(1,5000),new Account(2,11000),new Account(3,5400),new Account(4,-10),new Account(5,-50));
 
 //list of account 
-prinln("bank account list : ");
+println("bank account list : ");
 println(bank)
 
-val negAcc = (a: List[account]) => a.map( b => b.filter(x => x.balance < 0));
+val negAcc = (a: List[Account]) => a.filter(x => x.balance < 0);
 
 //list of account with negative balance
-prinln("bank account with negative balance :");
+println("\nbank account with negative balance :");
 println(negAcc(bank));
 
-val sumOfAccontBalance = (a:List[Account]) => a.reduce((b,c) => a.balance + c.balance);
+val sumOfAccontBalance = (a:List[Account]) => a.map(x => x.balance).reduce((b, c) => b + c);
 
 //sum of account balance 
 
-prinln("sum of bank accounts : ");
-prinln(sumOfAccountBalance(bank));
+println("\nsum of bank accounts : ");
+println(sumOfAccontBalance(bank));
 
-val interest = (a:List[Account]) => a.map(x => {
-    x.balance match{
-        case y if y >= 0 => x.deposit(y*0.05);
-        case _ => x.withdraw((Math.abs(y))*0.1);
-    }
-})
+val interest = ( a : List[Account] ) => a.map( x => {
+        x.balance match {
+            case b if b >= 0 => x.deposit(x.balance*0.05)
+            case _ => x withdraw(Math.abs(x.balance)* 0.1)
+
+        }
+        x
+    })
+
+println("\nfinal balance");
+println(interest(bank));
 
 
 
